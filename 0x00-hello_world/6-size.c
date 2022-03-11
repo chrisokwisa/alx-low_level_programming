c25d081fa273edc44efafea3a04b02f3bdb07202
chrisokwisa <okwisachristabel@gmail.com>
2022-03-11 11:16:37 +0000

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+long int i2;
+long long int i3;
+char c;
+float f;
+ 
+pritf("Size of a char. %lu byte(s)\n",(unsigned long) sizeof(c));
+printf("Size of an init: %lu byte(s)\n",(unsigned long long)sizeof(i));
+printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(i2));
+printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeofi3));
+printf("Size of a float: %lu byte(s)\n,(unsigned long)sizeof(f));
+return (o);
+}

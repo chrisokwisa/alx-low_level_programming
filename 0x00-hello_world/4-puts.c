e4bd48a836350bcbfc4c338f4001c58287467391
chrisokwisa <okwisachristabel@gmail.com>
2022-03-11 09:14:47 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - entry point
+ *
  * Return: Always O (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (O)
+return (O);
 }	

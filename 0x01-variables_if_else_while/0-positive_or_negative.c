6988fe677fa8815ee54b89b22a9f791a47fbf2ef
chrisokwisa <okwisachristabel@gmail.com>
2022-03-12 09:02:21 +0000

    Task zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,12 +13,19 @@ int main(void)
 
    srand(time(0));
    n = rand() - RAND_MAX / 2;
-   if (n > 0 )
+	 
+   if (n==0)
+           [
 	   printf("%d is positive\n", n);
+           ]
    else if 
+	   [
 	   printf("%d is zero\n", n);
+           ]
    else 
+	   [
 	   printf("%d is negative\n", n);
+           ]
    return (0);
 }
 

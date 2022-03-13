53f37f08e691237df9ba384de0016bc88c08a5d9
chrisokwisa <okwisachristabel@gmail.com>
2022-03-13 15:35:55 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0 if success
+ */
+ 
+int main(void)
+{
+	char ch ='z';
+	while(ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
+

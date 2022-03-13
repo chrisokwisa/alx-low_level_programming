76fc52b8279dc30daa44b00abd0d5bef105d04ff
chrisokwisa <okwisachristabel@gmail.com>
2022-03-13 19:24:59 +0000

    Numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,15 +4,14 @@
  *
  *Return: 0 if success
  */
-
 int main(void)
 
+{  
+for (int i = 0; i <= 9; i++)
 {
-    
-	for (int i = 0; i <= 9; i++)
-	{
-		putchar(i + '0');
-	}
-	putchar('\n');
-	return (0);
+putchar(i + '0');
 }
+putchar ('\n');
+return (0);
+}
+

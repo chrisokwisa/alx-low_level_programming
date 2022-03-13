43b45dd5a438ce284d5c5cbf92028f217e6dafa3
chrisokwisa <okwisachristabel@gmail.com>
2022-03-13 14:15:08 +0000

    What

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,17 +5,19 @@
  *Return: 0 if success
  */
 int main(void)
-{
-    char ch = 'a';
+
+{ 
+       char ch = 'a';
 	while(ch <= 'z')
 	{
-		if (ch != 'e' && ch != 'q')
-		{
-			putchar(ch);
-		}
-		ch++;
+	if (ch != 'e' && ch != 'q')
+	{
+	putchar(ch);
+	}
+	ch++;
 	}
 	putchar('\n');
 
     return (0);
 }
+

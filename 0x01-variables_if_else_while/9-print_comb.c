7e53c4e0a2adacb2852cc7fb436ca62b4a532f88
chrisokwisa <okwisachristabel@gmail.com>
2022-03-13 20:07:55 +0000

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,18 @@
  */
 int main(void)
 
+{   
+for (int i = 0; i <= 9; i++)
 {
-    
-	for (int i = 0; i <= 9; i++)
-	{
-		putchar(i + '0');
-		if (i != 9) {
-		    putchar(',');
-		    putchar(' ');
-		}
-	}
-	putchar('\n');
-
-    return 0;
+putchar(i + '0');
+if (i !=
+putchar(',');
+putchar(' ');
 }
+}
+putchar('\n');
+
+return (0);
+
+}
+

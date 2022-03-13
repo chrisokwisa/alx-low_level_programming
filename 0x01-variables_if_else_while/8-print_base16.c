79bcec81cdd98c89eda25c3f199780041fcaf01b
chrisokwisa <okwisachristabel@gmail.com>
2022-03-13 19:38:19 +0000

    Number eight

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0 if success
+ */
+int main(void)
+
+	char ch = 'a';
+	int i = 0;
+
+	while (i < 16)
+        {
+		if (i > 10)
+		{
+			putchar(i + 0);
+		}
+		else
+		{
+			putchar(i + 0);
+			ch++;
+		}
+		i++;
+	}
+        putchar('\n');
+    return (0);
+}
+

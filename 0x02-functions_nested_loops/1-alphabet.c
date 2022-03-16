83ea063016eb915625dcd80fbf939555ef6a3455
chrisokwisa <okwisachristabel@gmail.com>
2022-03-16 07:52:50 +0000

    Task one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: 0 if success
+ */
+
+void print_alphabet(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n')
+
+}
+
+	

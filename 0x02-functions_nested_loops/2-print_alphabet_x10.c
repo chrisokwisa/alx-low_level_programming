7389b8069058a372300208aa0aa38bea494f7fd7
chrisokwisa <okwisachristabel@gmail.com>
2022-03-16 10:06:36 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * add -prints add two numbers
- *@i: print int i
- *@k: print int k
- * Return: Always 0.
- */
-
-int add(int i, int k)
+ *  * print_alphabet_x10 - print alphabet
+ *   *
+ *    * Return: Always 0.
+ *     */
+void print_alphabet_x10(void)
 {
-return (i + k);
+	int alphabet;
+	int count;
+
+
+	count = 0;
+	while (count < 10)
+	{
+		for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+
+		count++;
+		_putchar('\n');
+	}
+
+
 }
+

d050c4e7d14f11084e612b8a70d8a91b16a48736
chrisokwisa <okwisachristabel@gmail.com>
2022-03-16 10:10:45 +0000

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * print_alphabet_x10 - print alphabet
- *   *
- *    * Return: Always 0.
- *     */
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: Always 0.
+ */         
 void print_alphabet_x10(void)
 {
 	int alphabet;

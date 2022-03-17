23b08c8a93b99f9bdee70036636e3aea43e309a2
chrisokwisa <okwisachristabel@gmail.com>
2022-03-17 20:34:49 +0000

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - prints nembers from zero to nine.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int i = '0';
+
+for (; i <= '9'; i++)
+_putchar(i);
+_putchar('\n');
+}
+

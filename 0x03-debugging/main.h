1874df5688d306ca918ec07af48a6f019afd4504
chrisokwisa <okwisachristabel@gmail.com>
2022-03-17 09:26:24 +0000

    Task zero

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,10 @@
-#include "main.h"
-/**
- * main - main
- * Return: 0
- */
+#ifndef MAIN_H
+#define MAIN_H
 
-int main(void)
-{
-int i;
-i = 0;
+#include <stdio.h>
 
-positive_or_negative(i);
-
-return (0);
-
-}
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+#endif

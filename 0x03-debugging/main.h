4865d0e1a404f34621c761e1058fe6b445e76139
chrisokwisa <okwisachristabel@gmail.com>
2022-03-17 09:20:09 +0000

    My commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,19 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-#include <stdio.h>
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int days);
+int main(void)
+{
+        int i;
 
-#endif
+        i = 98;
+        positive_or_negative(i);
 
-
-
-
-
-
-
-
+        return (0);
+}

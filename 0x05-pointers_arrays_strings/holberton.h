585c27c3a3adb6ca3819b71795b0917c7462da6b
chrisokwisa <99495858+chrisokwisa@users.noreply.github.com>
2022-03-21 23:45:21 +0300

    Update holberton.h

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,3 @@
-
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,5 +10,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
-#endif

5227a2c84a157faa1ce30266f73afd5261f88a95
chrisokwisa <okwisachristabel@gmail.com>
2022-03-21 10:50:39 +0000

    Testing my commit

---
+++ 0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*main - function that prints to an int as a parameter updates value to 98.
+* followed by a new line \n
+ * Return: Always 0.
+ */
+void reset_to_98(int *n);
+
+{
+	int n;
+
+	n = 402;
+	printf("Address of var variable: %d\n", &var);
+	reset_to_98(&n);
+	return (0);
+
+}
+

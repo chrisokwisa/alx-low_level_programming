639fd2c72a5d172656db96034a4d63e3ab9f1571
chrisokwisa <okwisachristabel@gmail.com>
2022-03-21 11:01:22 +0000

    Commit

--- 0-reset_to_98.c
+++ 0-reset_to_98.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 *main - function that prints to an int as a parameter updates value to 98.
@@ -9,9 +8,10 @@
 void reset_to_98(int *n);
 
 {
-	int n;
+	int n = INT_MAX / n = INT_MIN
 
 	n = 402;
+	n = -4096;
 	printf("Address of var variable: %d\n", &var);
 	reset_to_98(&n);
 	return (0);

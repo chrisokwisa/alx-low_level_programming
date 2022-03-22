056ab651985918809ef966173e817ec4d42f5629
chrisokwisa <99495858+chrisokwisa@users.noreply.github.com>
2022-03-22 12:42:27 +0300

    Update _putchar.c

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -2,23 +2,11 @@
 
 /**
  *_putchar - writes the character c to stdout
-<<<<<<< HEAD
- *@: The character to print
- *
- * Return: on success 1.
- * On error, -1 is returned, and error is there returned
-=======
  *@c: The character to print
- *
  * Return: on success 1.
  *On error, -1 is returned, and error is there returned
->>>>>>> 2c76dc74a703a2ff618a8ee66007a905f97fdd66
  */
 int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 2c76dc74a703a2ff618a8ee66007a905f97fdd66

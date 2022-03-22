ee575e273783cd1f1ede89897bf3a16549a0f3e8
chrisokwisa <okwisachristabel@gmail.com>
2022-03-22 13:28:04 +0000

    My 7th commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: A pointer to an int that will be changed
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+
+{
+int i, last;
+while (str[i] != '\0')
+{
+i++;
+}
+
+last = (i + 1) / 2;
+
+for (i = last; str[i]; i++)
+{
+_putchar (str[i]);
+}
+_putchar ('\n');
+
+}
+

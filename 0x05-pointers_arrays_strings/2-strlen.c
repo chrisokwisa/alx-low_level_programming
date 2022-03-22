3ee75d09b9f5d9389c01b68f78e6005c96ee6c62
chrisokwisa <okwisachristabel@gmail.com>
2022-03-22 07:47:35 +0000

    My commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strlen - length of a string
+ *@s: A pointer to an int that will be changed
+ *
+ * Return: void
+ */
+
+int _strlen(char *s)
+
+{
+int u;
+
+u = 0;
+while (s[u] != '\0')
+{
+u++;
+}
+return (u);
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,5 @@
-
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
+int _putchar(char c);
+void swap_int(int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -13,5 +9,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
-#endif

b0a50531a77b9bf1922d458446252f1885528f9e
chrisokwisa <okwisachristabel@gmail.com>
2022-03-30 11:53:40 +0000

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,41 +1,47 @@
 #include "main.h"
-int _functiond(int y, int x);
+
 /**
- * is_palindrome - function
- *Return: valve 0
- *@s: variable
+ * palind2 - obtains length of a
+ * @a: string
+ * @l: integer to count length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int palind2(char *a, int l)
+{
+	if (*a == 0)
+		return (l - 1);
+	return (palind2(a + 1, l + 1));
+}
+/**
+ * palind3 - compares string vs string reverse
+ * @a: string
+ * @l: length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int palind3(char *a, int l)
+{
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, l - 2));
+}
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to evaluate
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int is_palindrome(char *s)
 {
+	int l;
 
-	if (*s != '\0')
-	{
-		return (1);
-	}
-	return (0);
-}
-/**
- * _functiond - function
- * @x: variable
- * @y: variable
- * Return: functiond
- */
-int _functiond(int y, int x)
-{
-	if (x == 2)
-	{
-		return (1);
-	}
-	if (x <= 1)
-	{
-		return (0);
-	}
-	if (y % x == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		return (_functiond(y, x - 1));
-	}
+	l = palind2(s, 0);
+	return (palind3(s, l));
 }

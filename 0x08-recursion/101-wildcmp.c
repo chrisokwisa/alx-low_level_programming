4b0200dcbf993bdc64fd5f7c0709e040e028210d
chrisokwisa <okwisachristabel@gmail.com>
2022-03-30 11:49:19 +0000

    Task eight

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,42 +1,28 @@
 #include "main.h"
-int _functiond(int y, int x);
 /**
- *wildcmp - function
- *Return: valve 0
- *@s1: variable
- *@s2: variabl::e
+ * wildcmp - compares two strings and returns 1 if the strings
+ * can be considered identical, otherwise return 0.
+ * @s1: string to compare to
+ * @s2: string with wild character
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int wildcmp(char *s1, char *s2)
 {
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 
-	if (s1 != s2)
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
 	{
-		return (1);
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+			return (0);
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
 	}
+
 	return (0);
-}
-/**
- * _functiond - function
- * @x: variable
- * @y: variable
- * Return: functiond
- */
-int _functiond(int y, int x)
-{
-	if (x == 2)
-	{
-		return (1);
-	}
-	if (x <= 1)
-	{
-		return (0);
-	}
-	if (y % x == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		return (_functiond(y, x - 1));
-	}
 }

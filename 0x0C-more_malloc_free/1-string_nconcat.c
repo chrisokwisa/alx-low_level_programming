e1e3b5237ffb224d69e318d63fc5c096b470e3eb
chrisokwisa <99495858+chrisokwisa@users.noreply.github.com>
2022-04-07 14:34:45 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * string_nconcat - Concatenates two strings using at
@@ -18,14 +18,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-  
-  	if (s2 == NULL)
+
+	if (s2 == NULL)
 		s2 = "";
 
 	for (index = 0; s1[index]; index++)
 		len++;
 
-	concat = malloc(sizeof(char) * (len + 1));
+	concat = malloc (sizeof(char) * (len + 1));
 
 	if (concat == NULL)
 		return (NULL);

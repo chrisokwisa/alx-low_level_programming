13a418c649079bc25a469d8c77654bc916cc9627
chrisokwisa <99495858+chrisokwisa@users.noreply.github.com>
2022-04-11 12:46:52 +0300

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,6 +3,7 @@
 
 /*
  * File: 3-function_like_macro.h
+ * auth: christabel
  * Desc: Header file that defines a function-like macro ABS(x)
  *       that computes the absolute value of a number x.
  */

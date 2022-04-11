5c84e120ae7eebdfb686007e9cc1bb948fef0176
chrisokwisa <okwisachristabel@gmail.com>
2022-04-11 09:25:18 +0000

    object like-macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ *
+ *Desc: Header file that defines a macro named SIZE
+ *as an abbreviation fo the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif

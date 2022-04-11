75b2d742f72ba11b4c4fa63ab9417a65bfb0f8a6
chrisokwisa <99495858+chrisokwisa@users.noreply.github.com>
2022-04-11 12:29:53 +0300

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,6 +2,7 @@
 #define OBJECT_LIKE_MACRO_H
 
 /**
+ *FILE NAME: 0-object_like_macro.h
  *Auth: christabel
  *Desc: Header file that defines a macro named SIZE
  *as an abbreviation fo the token 1024.

e003b8cbdfc28c7bc03c90b12cd6f96418bfccf0
chrisokwisa <okwisachristabel@gmail.com>
2022-05-04 11:11:18 +0000

    commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int num = 0;
 	char letter;
-
+	{
 	for (num = 0; num < 10; num++)
 		putchar((num + 10) + '0');
 
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
-
+	}
 	putchar('\n');
-	
+
 	return (0);
 }
 

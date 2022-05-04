b7e7b89ffa22f7b035f6a8d8d567ab83ed565f4c
chrisokwisa <okwisachristabel@gmail.com>
2022-05-04 12:37:33 +0000

    commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,7 +19,7 @@ int main(void)
 				putchar((digit2 % 10) + '0');
 				putchar((digit3 % 10) + '0');
 
-				if(digit1 == 7 && digit2 == 8 && digit3 == 9)
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
 
 				   putchar(',');
@@ -29,5 +29,5 @@ int main(void)
 	}
 		putchar('\n');
 
-		return (0);
+		return (0)`;
 }

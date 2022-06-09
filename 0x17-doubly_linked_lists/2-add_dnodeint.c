a603646db0c0fee39d2211b8fef69a398d495542
chrisokwisa <okwisachristabel@gmail.com>
2022-06-09 12:13:55 +0000

    Adds number of elements

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,7 +17,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new->n = n;
+	new->prev = NULL;
 	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
 
 	*head = new;
 

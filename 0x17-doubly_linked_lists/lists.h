9ce29f8c9200e4ba96fd9b15d8272db68b8e469f
Christabel Okwisa <99495858+chrisokwisa@users.noreply.github.com>
2022-06-09 14:35:37 +0300

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -29,4 +29,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif #lists_h
+#endif /*lists_h*/

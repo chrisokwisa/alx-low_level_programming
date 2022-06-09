c6798de0182ab45fceeb3764f668ec7e4cb982b4
chrisokwisa <okwisachristabel@gmail.com>
2022-06-09 15:22:35 +0000

    Get index at node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -6,7 +6,7 @@
  * @index: The node to locate.
  *
  * Return: If the node does not exist - NULL
- * 	Otherwise - the address of the located node.
+ *	Otherwise - the address of the located node.
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {

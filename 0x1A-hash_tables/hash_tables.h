22015ec305ae011505d718591c27d7d87e6379e5
chrisokwisa <okwisachristabel@gmail.com>
2022-06-30 09:58:35 +0000

    Adds an element to the hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -32,7 +32,7 @@ typedef struct hash_Table_s
 	hash_node_t **array;
 } hash_table_t;
 
-hash_table_t; *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_tabe_set(hash_table_t *ht, const char *key, const char *value);

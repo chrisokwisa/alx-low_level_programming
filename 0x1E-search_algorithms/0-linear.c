3cbcf0336459430be81610805f424c5ac1848fe1
chrisokwisa <okwisachristabel@gmail.com>
2022-10-04 01:24:20 -0700

    commit

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,16 +1,17 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of integers
- *		using the Linear search
- *@array: pointer to the first element of the array to search
- *@size: number of elements in array
- *@value: value to search for
- *Return: the first index where value is located
- *	if value is not present in array / if array is NULL, must return -1
- *Desc: Every time you compare a value in the array to the value you
- *	are searching you to print this value
- */
+  * linear_search - Searches for a value in an array
+  *                 of integers using linear search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the first index where the value is located.
+  *
+  * Description: Prints a value every time it is compared in the array.
+  */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
@@ -20,9 +21,10 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("value checked array[%ld] = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
+
 	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,25 +13,25 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, low, high;
+	size_t i, left, right;
 
 	if (array == NULL)
 		return (-1);
 
-	for (low = 0, high = low - 1; high >= low;)
+	for (left = 0, right = left - 1; right >= left;)
 	{
 		printf("searching in array: ");
-		for (i = low; i < high; i++)
+		for (i = left; i < right; i++)
 			printf("%d, ", array[i]);
 		printf("%d\n", array[i]);
 
-		i = low + (high - low) / 2;
+		i = left + (right - left) / 2;
 		if (array[i] == value)
 			return (i);
 		if (array[i] > value)
-			high = i - 1;
+			right = i - 1;
 		else
-			low = i + 1;
+			left = i + 1;
 	}
 	return (-1);
 }
